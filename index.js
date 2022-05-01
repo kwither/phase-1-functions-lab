88c67bc201eae99825d11f41d35328bcdca84a7f
Kevin Wither <kvn.wither@gmail.com>
2022-05-01 01:13:47 -0600

    commit tree baeaa448ad0c45bf6645c01b52b6810f6c7c06a9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    return Math.abs(number - 42);
+}
+
+function distanceFromHqInFeet(number) {
+    distanceFromHqInBlocks(number); {
+        return Math.abs(number - 42) * 264;
+    }
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    distanceTravelledInFeet(start, destination); {
+        let distance = Math.abs(destination - start) * 264;
+
+        if (distance >=1 && distance <=400) {
+            return distance * 0;
+        }else if (distance >=401 && distance <=2000) {
+            return (distance - 400) * .02;
+        }else if (distance >=2001 && distance <=2499) {
+            return 25;
+        }else {
+            return `cannot travel that far`;
+        }
+    }
+}
